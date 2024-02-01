b46df675dad05ba680cd6a4c2e1dba0aae56901f
KM <the.real.laplace@gmail.com>
2024-02-01 21:49:39 +0000

    uinput header

--- scripts/make.mjs
+++ scripts/make.mjs
@@ -8,21 +8,19 @@ execSync(`cd ${C.dir.libevdev} && make -j$(nproc)`, { stdio: 'inherit' })
 
 console.log("install to", C.dir.dist)
 await Fs.promises.rm(C.dir.dist, { recursive: true }).catch(() => {})
-await Fs.promises.mkdir(C.dir.dist, { recursive: true })
-
 await Promise.all([
-	Fs.promises.mkdir(Path.join(C.dir.dist, 'include')),
-	Fs.promises.mkdir(Path.join(C.dir.dist, 'lib')),
+	Fs.promises.mkdir(Path.join(C.dir.dist, 'include/libevdev'), { recursive: true }),
+	Fs.promises.mkdir(Path.join(C.dir.dist, 'lib'), { recursive: true }),
 ])
 
 await Promise.all([
 	Fs.promises.cp(
 		Path.join(C.dir.libevdev, 'libevdev/libevdev.h'),
-		Path.join(C.dir.dist, 'include/libevdev.h'),
+		Path.join(C.dir.dist, 'include/libevdev/libevdev.h'),
 	),
 	Fs.promises.cp(
 		Path.join(C.dir.libevdev, 'libevdev/libevdev-uinput.h'),
-		Path.join(C.dir.dist, 'include/libevdev-uinput.h'),
+		Path.join(C.dir.dist, 'include/libevdev/libevdev-uinput.h'),
 	),
 	Fs.promises.cp(
 		Path.join(C.dir.libevdev, 'libevdev/.libs/libevdev.a'),
