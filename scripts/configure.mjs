4f423b90a995f92f3f1178dca92975536cee7505
KM <the.real.laplace@gmail.com>
2026-08-17 13:31:27 +0300

    use global fetch and freedesktop tarballs

--- scripts/configure.mjs
+++ scripts/configure.mjs
@@ -3,10 +3,4 @@ import C from './util/common.js'
 
 console.log("configure build in", C.dir.libevdev)
 
-execSync([
-	`cd ${C.dir.libevdev}`,
-	'autoreconf --install --force',
-	'autoconf',
-	'automake',
-	'./configure',
-].join(' && '), { stdio: 'inherit' })
+execSync(`cd ${C.dir.libevdev} && ./configure`, { stdio: 'inherit' })
